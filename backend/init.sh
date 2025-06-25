6fc59343c420a6e2281edac25ece124d7e885aef
Lukáš Homola <lukas-homola@seznam.cz>
2025-06-25 23:59:17 +0200

    Adjustments for the deployment.

--- backend/init.sh
+++ backend/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "📥 Importing JSON data..."
 
-mongoimport --db realestate --collection estatetypes --file /data/data_default/estatetypes.json --jsonArray
-mongoimport --db realestate --collection regions --file /data/data_default/regions.json --jsonArray
+mongoimport --db realestate --collection estatetypes --file /data/estatetypes.json --jsonArray
+mongoimport --db realestate --collection regions --file /data/regions.json --jsonArray
 
 echo "✅ Import completed."
