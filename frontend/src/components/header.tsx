f1d45c0ef21856ad66454477f4550711503f1408
Lukáš Homola <lukas-homola@seznam.cz>
2025-06-25 18:38:48 +0200

    Basic mobile nav
    At the end, adjusted mobile nav with simple responsive version

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,18 +1,19 @@
 import { NavLink } from "react-router-dom";
-
 import "../css/Header.css";
 
 function Header() {
   return (
-    <>
-      <header>
-        <div className="header_container">
-          <div className="header_container_left">
+    <header>
+      <div className="header_container">
+        <details>
+          <summary>☰ </summary>
+          <div className="header_flex">
             <section>
               <h3>Reas</h3>
               <p className="font-caption">interview projekt</p>
             </section>
-            <nav>
+
+            <nav className="nav">
               <NavLink
                 to="/"
                 className={({ isActive }) => (isActive ? "nav_active" : "")}
@@ -33,13 +34,47 @@ function Header() {
                 Ceny nemovitostí
               </NavLink>
             </nav>
+
+            <section>
+              <p>Lukáš homola</p>
+            </section>
           </div>
+        </details>
+      </div>
+
+      <div className="header_container_desktop">
+        <div className="header_container_left_desktop">
           <section>
-            <p>Lukáš homola</p>
+            <h3>Reas</h3>
+            <p className="font-caption">interview projekt</p>
           </section>
+          <nav className="desktop_nav">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "nav_active" : "")}
+              end
+            >
+              Domů
+            </NavLink>
+            <NavLink
+              to="/chci-nabidku"
+              className={({ isActive }) => (isActive ? "nav_active" : "")}
+            >
+              Získat nabídku
+            </NavLink>
+            <NavLink to="/" className="nav_placeholder">
+              Kupuji
+            </NavLink>
+            <NavLink to="/" className="nav_placeholder">
+              Ceny nemovitostí
+            </NavLink>
+          </nav>
         </div>
-      </header>
-    </>
+        <section>
+          <p>Lukáš homola</p>
+        </section>
+      </div>
+    </header>
   );
 }
 
